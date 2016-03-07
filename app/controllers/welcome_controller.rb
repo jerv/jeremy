04e9809b34632479c4caafaae09fcd8ae790f2d9
Jeremy Venegas <jeremymvenegas@gmail.com>
2016-03-07 15:47:23 -0800

    styling fixes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
 	def index
-		@posts = Post.all.limit(3).order("created_at DESC")
-		@projects = Project.all.limit(3).order("created_at DESC")
+		@posts = Post.all.limit(5).order("created_at DESC")
+		@projects = Project.all.limit(5).order("created_at DESC")
 	end
 end
